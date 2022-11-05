39840d5cd425533620bc3ef5c90e185e1df5fd1c
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2022-11-05 22:38:05 +0700

    push midterm

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,35 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Models\Slide;
-use App\Models\Product;
+use App\Models\Products;
+use App\Models\Type_products;
 
 class PageController extends Controller
 {
     public function getIndex()
     {
         $slide = Slide::all();
-        $new_product = Product::where('new', 1)->get();
-        return view('page.trangchu', compact('slide', 'new_product'));
+        $new_product = Products::where('new',1)->paginate(8);
+        $top_product = Products::where('promotion_price','<>',0)->paginate(4);
+        return view('page.homepage', compact('slide','new_product', 'top_product'));
     }
 
-    public function getLoaiSp()
-    {
-        return view('page.loai_sanpham');
+    public function getProductType($type){
+        $type_name = Type_products::where('id',$type)->first();
+        $types = Type_products::all();
+        $type_product = Products::all();
+        $product_filter = Products::where('id_type',$type)->get();
+        $suggestion = Products::where('id_type', '<>', $type)->take(3)->get();
+        return view('page.product_type', compact( 'type_name','types','type_product', 'product_filter', 'suggestion'));
     }
-
-    public function getChitiet()
-    {
-        return view('page.chitiet_sanpham');
+    public function getProductDetail(){
+        return view('page.product_detail');
     }
-
-    public function getLienhe()
-    {
-        return view('page.lienhe');
+    public function getContact(){
+        return view('page.contact');
     }
-
-    public function getAbout()
-    {
+    public function getAbout(){
         return view('page.about');
     }
 }
--- app/Models/News.php
+++ app/Models/News.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Slide extends Model
+class News extends Model
 {
     use HasFactory;
-    protected $table = 'slide';
+    protected $table = 'news';
 }
